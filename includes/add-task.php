07fc4c45e152cb40e7668ad3c76349d2c357daf9
Guillermo Hermosillo <g.andrewhermosillo@gmail.com>
2015-04-17 11:44:00 -0700

    finished milestone 12

--- includes/add-task.php
+++ includes/add-task.php
@@ -10,6 +10,15 @@
 
 	$query = "SELEST * FROM tasks WHERE task='$task' and date='$date', and time='$time'";
 
-	
+	if ($result = mysqli->query($query)) {
+		while ($row = $result->fetch_assoc()) {
+			$task_id = $row['id'];
+			$task_name = $row['task']
+		}
+	}
+
+	$mysqli->close();
+
+	echo '<li><spam>'.task_name.'</spam><img id="'.$task_id.'" class="delete-button" width="10px" src="images/close.svg" /><?li>';
 
 ?>
